4683ecd5d79346264a1509fee9db8961dc5be7a5
Alvaro Colunga <alvaro.colunga@gmail.com>
2015-07-24 16:29:02 +0000

    Multiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,11 @@
 var models = require('../models/model.js');
 
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz: quiz});
+	})
+};
+
 exports.question = function(req,res){
 models.Quiz.findAll().success(function(quiz){
 	res.render('quizes/question',{pregunta:quiz[0].pregunta})
@@ -8,11 +14,17 @@ models.Quiz.findAll().success(function(quiz){
 };
 
 exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if(req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta:'Correcto'});			
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if(req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer',{quiz:quiz,respuesta:'Correcto'});			
 		}else{
-			res.render('quizes/answer',{respuesta:'Incorrecto'});
+			res.render('quizes/answer',{quiz:quiz,respuesta:'Incorrecto'});
 		}
+	})
+};
+
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index',{quizes:quizes});
 	})
 };
