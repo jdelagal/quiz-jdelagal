17846d86775ea7cda7a21624b0b77ade9bb1428e
Alvaro Colunga <alvaro.colunga@gmail.com>
2015-07-25 21:14:34 +0000

    Se agrego validacion de preguntas y actualizacion de sequelize a 2.0.0 para funcion de validate

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,7 +13,7 @@ exports.load = function(req,res,next,quizId){
 
 exports.show = function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: req.quiz});
+		res.render('quizes/show',{quiz: req.quiz,errors:[]});
 	})
 };
 
@@ -31,7 +31,7 @@ exports.answer = function(req,res){
 		resultado = 'Correcto';
 	}
 
-	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado});
+	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado,errors:[]});
 };
 
 exports.index = function(req,res){
@@ -43,11 +43,11 @@ exports.index = function(req,res){
 		busqueda=busqueda.replace(" ","%");
 
 		models.Quiz.findAll({where:["pregunta like ?",busqueda]}).then(function(quizes){
-			res.render('quizes/index',{quizes:quizes});
+			res.render('quizes/index',{quizes:quizes,errors:[]});
 		})
 	}else{
 		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index',{quizes:quizes});
+			res.render('quizes/index',{quizes:quizes,errors:[]});
 		})
 	}
 };
@@ -56,13 +56,19 @@ exports.index = function(req,res){
 exports.new = function(req,res){
 	var quiz = models.Quiz.build({pregunta:"Pregunta",respuesta:"Respuesta"});
 
-	res.render('quizes/new',{quiz:quiz});
+	res.render('quizes/new',{quiz:quiz,errors:[]});
 };
 
 exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
 
-	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})
+	quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new',{quiz:quiz,errors:err.errors});
+		}else{
+			quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			})
+		}
+	});
 };
